82b4a4c7c3a97bdf36248811b85c5f50695c4570
VladimirShalamanov <vladi.gesh@gmail.com>
2023-11-14 18:13:58 +0200

    Created game service

--- client/src/components/game-create/GameCreate.jsx
+++ client/src/components/game-create/GameCreate.jsx
@@ -1,7 +1,22 @@
 export default function GameCreate() {
+    const createGameSubmitHandler = (e) => {
+        e.preventDefault();
+
+        // const {
+        //     title,
+        //     category,
+        //     maxLevel,
+        //     imageUrl,
+        //     summary
+        // } 
+        const gameData = Object.fromEntries(new FormData(e.currentTarget));
+
+        console.log(gameData);
+    }
+
     return (
         <section id="create-page" className="auth">
-            <form id="create">
+            <form id="create" onSubmit={createGameSubmitHandler}>
                 <div className="container">
 
                     <h1>Create Game</h1>
