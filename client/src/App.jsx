022c32602b29d554d3fec50d9268ed3066b17ed6
VladimirShalamanov <vladi.gesh@gmail.com>
2023-11-14 17:36:26 +0200

    Add game-create component
    clean App function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,9 @@ import { Routes, Route } from 'react-router-dom';
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import GameList from './components/game-list/GameList';
+import GameCreate from './components/game-create/GameCreate';
 
-function App() {
+export default function App() {
     return (
         <div id="box">
             <Header />
@@ -12,11 +13,10 @@ function App() {
             <Routes>
                 <Route path='/header' element={<Header />} />
                 <Route path='/games' element={<GameList />} />
+                <Route path='/games/create' element={<GameCreate />} />
             </Routes>
 
             <Home />
         </div>
     )
 }
-
-export default App
