0784a167899c3257276fe928e4c59ebf2c16f685
VladimirShalamanov <vladi.gesh@gmail.com>
2023-11-15 15:09:39 +0200

    Details page show dynamic

--- client/src/components/game-details/GameDetails.jsx
+++ client/src/components/game-details/GameDetails.jsx
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import * as gameService from "../../services/gameService";
+
 export default function GameDetails() {
+    const { gameId } = useParams();
+    const [game, setGame] = useState({});
+
+    useEffect(() => {
+        gameService.getOne(gameId)
+            .then(setGame);
+    }, [gameId]);
     return (
         <section id="game-details">
             <h1>Game Details</h1>
@@ -6,18 +18,13 @@ export default function GameDetails() {
             <div className="info-section">
 
                 <div className="game-header">
-                    <img className="game-img" src="images/MineCraft.png" />
-                    <h1>Bright</h1>
-                    <span className="levels">MaxLevel: 4</span>
-                    <p className="type">Action, Crime, Fantasy</p>
+                    <img className="game-img" src={game.imageUrl} alt={game.title} />
+                    <h1>{game.title}</h1>
+                    <span className="levels">{game.maxLevel}</span>
+                    <p className="type">{game.category}</p>
                 </div>
 
-                <p className="text">
-                    Set in a world where fantasy creatures live side by side with humans. A human cop is forced to work
-                    with an Orc to find a weapon everyone is prepared to kill for. Set in a world where fantasy
-                    creatures live side by side with humans. A human cop is forced
-                    to work with an Orc to find a weapon everyone is prepared to kill for.
-                </p>
+                <p className="text">{game.summary}</p>
 
                 {/* <!-- Bonus ( for Guests and Users ) -->
                 <div className="details-comments">
--- client/src/components/game-list/GameList.jsx
+++ client/src/components/game-list/GameList.jsx
@@ -15,7 +15,7 @@ export default function GameList() {
         <section id="catalog-page">
             <h1>All Games</h1>
 
-            {games.map(g => (<GameListItem key={g._id} {...g} />))}
+            {games.map(g => <GameListItem key={g._id} {...g} />)}
 
             {games.length == 0
                 && <h3 className="no-articles">No articles yet</h3>
