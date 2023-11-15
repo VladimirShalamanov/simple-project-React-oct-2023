065731a599da6b2701a2bf84bf349d8cc95f3e8f
VladimirShalamanov <vladi.gesh@gmail.com>
2023-11-15 14:48:56 +0200

    Show static details for game
    Details page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import GameList from './components/game-list/GameList';
 import GameCreate from './components/game-create/GameCreate';
 import Login from './components/login/Login';
 import Register from './components/register/Register';
+import GameDetails from './components/game-details/GameDetails';
 
 export default function App() {
     return (
@@ -18,6 +19,7 @@ export default function App() {
                 <Route path='/games/create' element={<GameCreate />} />
                 <Route path='/login' element={<Login />} />
                 <Route path='/register' element={<Register />} />
+                <Route path='/games/:gameId' element={<GameDetails />} />
             </Routes>
 
             <Home />
