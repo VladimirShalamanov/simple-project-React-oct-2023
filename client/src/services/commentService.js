0ea1dd8f730b3843eaa716c25a0c5a2811423053
VladimirShalamanov <vladi.gesh@gmail.com>
2023-11-16 13:51:03 +0200

    Show comments for certain game

--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -2,10 +2,19 @@ import * as request from "../lib/request";
 
 const baseUrl = 'http://localhost:3030/jsonstore/commets';
 
-export const getAll = async () => {
+export const getAll = async (gameId) => {
+    /* For filter work with the server */
+
+    // const query = new URLSearchParams({
+    //     where: `gameId="${gameId}"`
+    // });
+
+    // const result = await request.get(`${baseUrl}?${query.toString()}`);
+
     const result = await request.get(baseUrl);
 
-    return Object.values(result);
+    // temp solution until migration
+    return Object.values(result).filter(c => c.gameId === gameId);
 }
 
 export const create = async (gameId, username, text) => {
