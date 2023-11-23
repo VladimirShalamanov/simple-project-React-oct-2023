ec1be353d95fc62d666a772c07ba0a9fae30d343
VladimirShalamanov <vladi.gesh@gmail.com>
2023-11-23 20:38:29 +0200

    Add comment

--- client/src/HOC/withAuth.jsx
+++ client/src/HOC/withAuth.jsx
@@ -2,6 +2,9 @@ import { useContext } from "react";
 
 import AuthContext from "../contexts/authContext";
 
+// Important !! - This is alternative way to use auth values
+// in 'HOC'. The correct way is to use hooks. Do NOT use it for your project.
+
 export default function withAuth(Component) {
     const EnhancedComponent = (props) => {
         const auth = useContext(AuthContext);
