f7a39e62db71223f35ceea4afdd5665bef35a1e5
VladimirShalamanov <vladi.gesh@gmail.com>
2023-11-24 15:14:13 +0200

    Fix base url

--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -1,6 +1,6 @@
 import * as request from "../lib/request";
 
-const baseUrl = 'http://localhost:3030/data/commets';
+const baseUrl = 'http://localhost:3030/data/comments';
 
 export const getAll = async (gameId) => {
     /* For filter work with the server */
