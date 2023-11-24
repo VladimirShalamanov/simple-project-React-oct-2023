91c3bc88a8a7f1e69cd190341b77c998a93325d0
VladimirShalamanov <vladi.gesh@gmail.com>
2023-11-24 18:17:19 +0200

    Add Game edit to root

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Home from "./components/home/Home";
 import GameList from './components/game-list/GameList';
 import GameCreate from './components/game-create/GameCreate';
 import GameDetails from './components/game-details/GameDetails';
+import GameEdit from './components/game-edit/GameEdit';
 
 import Register from './components/register/Register';
 import Login from './components/login/Login';
@@ -25,6 +26,7 @@ export default function App() {
                     <Route path={Path.GameList} element={<GameList />} />
                     <Route path={Path.GameCreate} element={<GameCreate />} />
                     <Route path={Path.GameDetails} element={<GameDetails />} />
+                    <Route path={Path.GameEdit} element={<GameEdit />} />
 
                     <Route path={Path.Register} element={<Register />} />
                     <Route path={Path.Login} element={<Login />} />
