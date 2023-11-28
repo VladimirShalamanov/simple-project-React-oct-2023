e3be05064f65474cd3ccf662a19121837c1cdab7
VladimirShalamanov <vladi.gesh@gmail.com>
2023-11-28 11:24:04 +0200

    Add Error Boundary

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,28 +13,31 @@ import GameEdit from './components/game-edit/GameEdit';
 import Register from './components/register/Register';
 import Login from './components/login/Login';
 import Logout from './components/logout/Logout';
+import ErrorBoundary from './components/ErrorBoundary';
 
 // Do not use all advanced tools from this workshop (only some)
 
 export default function App() {
     return (
-        <AuthProvider>
-            <div id="box">
-                <Header />
+        <ErrorBoundary>
+            <AuthProvider>
+                <div id="box">
+                    <Header />
 
-                <Routes>
-                    <Route path={Path.Home} element={<Home />} />
-                    <Route path={Path.Header} element={<Header />} />
-                    <Route path={Path.GameList} element={<GameList />} />
-                    <Route path={Path.GameCreate} element={<GameCreate />} />
-                    <Route path={Path.GameDetails} element={<GameDetails />} />
-                    <Route path={Path.GameEdit} element={<GameEdit />} />
+                    <Routes>
+                        <Route path={Path.Home} element={<Home />} />
+                        <Route path={Path.Header} element={<Header />} />
+                        <Route path={Path.GameList} element={<GameList />} />
+                        <Route path={Path.GameCreate} element={<GameCreate />} />
+                        <Route path={Path.GameDetails} element={<GameDetails />} />
+                        <Route path={Path.GameEdit} element={<GameEdit />} />
 
-                    <Route path={Path.Register} element={<Register />} />
-                    <Route path={Path.Login} element={<Login />} />
-                    <Route path={Path.Logout} element={<Logout />} />
-                </Routes>
-            </div>
-        </AuthProvider>
+                        <Route path={Path.Register} element={<Register />} />
+                        <Route path={Path.Login} element={<Login />} />
+                        <Route path={Path.Logout} element={<Logout />} />
+                    </Routes>
+                </div>
+            </AuthProvider>
+        </ErrorBoundary>
     );
 };
